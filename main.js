6a0580b56adbf6d5c652e1b83509858d62b53d0c
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-04-20 14:08:33 -0500

    first commit with location

---
+++ main.js
@@ -0,0 +1,6 @@
+import {getZip, useOut} from "./manager.js"
+
+
+console.log(getZip())
+
+console.log(useOut)
---
+++ manager.js
@@ -0,0 +1,48 @@
+let out = {}
+const message = document.querySelector('#message');
+const btn = document.querySelector('#show');
+const successfulLookup = position => {
+    const { latitude, longitude } = position.coords;
+    return fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=758dfe53035e47849a3b369f1110df2c`)
+        .then(response => response.json())
+        .then(console.log, "line 8")
+        .then(res => out=res);
+}
+export const useOut = () =>{
+    return {...out}
+}
+export const getZip = () => {
+
+    // check if the Geolocation API is supported
+    if (!navigator.geolocation) {
+        message.textContent = `Your browser doesn't support Geolocation`;
+        message.classList.add('error');
+        return;
+    }
+}
+// handle click event
+btn.addEventListener('click', function () {
+    // get the current position
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    console.log(useOut())
+});
+
+
+// handle success case
+const  onSuccess= (position) =>{
+    const {
+        latitude,
+        longitude
+    } = position.coords;
+    let output = successfulLookup(position).results
+    message.classList.add('success');
+    message.textContent = `Your location: (${latitude},${longitude})`;
+    console.log(output, "out put")
+    return output
+}
+
+// handle error case
+const onError = () => {
+    message.classList.add('error');
+    message.textContent = `Failed to get your location!`;
+}
